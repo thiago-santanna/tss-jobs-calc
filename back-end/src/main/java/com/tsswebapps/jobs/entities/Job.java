7d62776c6d76069836df084d339e5ccaa3fdd367
xthiagosant <uzmaki_thiago@outlook.com>
2022-03-22 12:16:32 -0300

    camada de entidades

---
+++ back-end/src/main/java/com/tsswebapps/jobs/entities/Job.java
@@ -0,0 +1,138 @@
+package com.tsswebapps.jobs.entities;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Job {
+	@Id
+	@GeneratedValue
+	private UUID id;
+	
+	private String nome;
+	
+	private String empresaSolicitante;
+	
+	private String descricao;
+	
+	private int horasDia;
+	
+	private int horasEstimadaJob;
+	
+	private BigDecimal valorEstimadoJob;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmpresaSolicitante() {
+		return empresaSolicitante;
+	}
+
+	public void setEmpresaSolicitante(String empresaSolicitante) {
+		this.empresaSolicitante = empresaSolicitante;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getHorasDia() {
+		return horasDia;
+	}
+
+	public void setHorasDia(int horasDia) {
+		this.horasDia = horasDia;
+	}
+
+	public int getHorasEstimadaJob() {
+		return horasEstimadaJob;
+	}
+
+	public void setHorasEstimadaJob(int horasEstimadaJob) {
+		this.horasEstimadaJob = horasEstimadaJob;
+	}
+
+	public BigDecimal getValorEstimadoJob() {
+		return valorEstimadoJob;
+	}
+
+	public void setValorEstimadoJob(BigDecimal valorEstimadoJob) {
+		this.valorEstimadoJob = valorEstimadoJob;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Job other = (Job) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+	}
+
+	@Override
+	public String toString() {
+		return "Job [id=" + id + ", nome=" + nome + "]";
+	}
+	
+	public Job() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Job(UUID id, String nome, String empresaSolicitante, String descricao, int horasDia, int horasEstimadaJob,
+			BigDecimal valorEstimadoJob, User user) {
+		this.id = id;
+		this.nome = nome;
+		this.empresaSolicitante = empresaSolicitante;
+		this.descricao = descricao;
+		this.horasDia = horasDia;
+		this.horasEstimadaJob = horasEstimadaJob;
+		this.valorEstimadoJob = valorEstimadoJob;
+		this.user = user;
+	}
+	
+	
+}
---
+++ back-end/src/main/java/com/tsswebapps/jobs/entities/User.java
@@ -0,0 +1,173 @@
+package com.tsswebapps.jobs.entities;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private UUID id;
+
+	private String nome;
+
+	private String email;
+
+	private String senha;
+
+	private BigDecimal valorHora;
+
+	private BigDecimal faturarPorMes;
+
+	private int diasTrabalharSemana;
+
+	private int horasTrabalharDia;
+
+	private int femanasFeriasAno;
+
+	private String urlFoto;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Job> jobs = new ArrayList<>();
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public BigDecimal getValorHora() {
+		return valorHora;
+	}
+
+	public void setValorHora(BigDecimal valorHora) {
+		this.valorHora = valorHora;
+	}
+
+	public BigDecimal getFaturarPorMes() {
+		return faturarPorMes;
+	}
+
+	public void setFaturarPorMes(BigDecimal faturarPorMes) {
+		this.faturarPorMes = faturarPorMes;
+	}
+
+	public int getDiasTrabalharSemana() {
+		return diasTrabalharSemana;
+	}
+
+	public void setDiasTrabalharSemana(int diasTrabalharSemana) {
+		this.diasTrabalharSemana = diasTrabalharSemana;
+	}
+
+	public int getHorasTrabalharDia() {
+		return horasTrabalharDia;
+	}
+
+	public void setHorasTrabalharDia(int horasTrabalharDia) {
+		this.horasTrabalharDia = horasTrabalharDia;
+	}
+
+	public int getFemanasFeriasAno() {
+		return femanasFeriasAno;
+	}
+
+	public void setFemanasFeriasAno(int femanasFeriasAno) {
+		this.femanasFeriasAno = femanasFeriasAno;
+	}
+
+	public String getUrlFoto() {
+		return urlFoto;
+	}
+
+	public void setUrlFoto(String urlFoto) {
+		this.urlFoto = urlFoto;
+	}
+
+	public List<Job> getJobs() {
+		return jobs;
+	}
+
+	public void setJobs(List<Job> jobs) {
+		this.jobs = jobs;
+	}
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(UUID id, String nome, String email, String senha, BigDecimal valorHora, BigDecimal faturarPorMes,
+			int diasTrabalharSemana, int horasTrabalharDia, int femanasFeriasAno, String urlFoto, List<Job> jobs) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.valorHora = valorHora;
+		this.faturarPorMes = faturarPorMes;
+		this.diasTrabalharSemana = diasTrabalharSemana;
+		this.horasTrabalharDia = horasTrabalharDia;
+		this.femanasFeriasAno = femanasFeriasAno;
+		this.urlFoto = urlFoto;
+		this.jobs = jobs;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+	}
+	
+	
+}
