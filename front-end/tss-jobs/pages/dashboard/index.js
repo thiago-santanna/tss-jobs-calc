d804177be1a0ac0ecddeb0aab6fa2871a571215b
xthiagosant <uzmaki_thiago@outlook.com>
2022-03-26 11:38:12 -0300

    responsividade da dashboard

--- front-end/tss-jobs/pages/dashboard/index.js
+++ front-end/tss-jobs/pages/dashboard/index.js
@@ -7,9 +7,9 @@ const Dashboard = () => {
   return (
     <>
       <NavBarComponent />
-      <div className="bg-gray-800 text-white border-t border-gray-200 h-32">
+      <div className="bg-gray-800 text-white border-t border-gray-200 h-48 md:h-32">
         <div className="container mx-auto">
-          <div className="flex flex-row flex-wrap space-x-3 justify-between p-2 items-end">
+          <div className="flex flex-row flex-wrap gap-2 md:gap-0 md:space-x-3 justify-center md:justify-between p-2 md:items-end">
             <div>
               <p>12</p>
               <span>Projetos ao total</span>
@@ -36,23 +36,23 @@ const Dashboard = () => {
       </div>
 
       <div className="container mx-auto -m-8">
-        <div className="bg-slate-100 flex flex-col md:flex-row space-x-4 justify-between items-center p-4 rounded-md mb-4">
-          <div>
-            <span className="mx-4 text-slate-400 ">1</span>
+        <div className="bg-slate-100 flex flex-col md:flex-row md:space-x-4 md:justify-between items-center p-4 rounded-md mb-4">
+          <div className="mb-2">
+            <span className="md:mx-4 px-1 text-slate-400 ">1</span>
             <span className="font-bold">Pizzaria Guloso</span>
           </div>
 
-          <div>
+          <div className="mb-2">
             <p className="text-slate-400">Prazo</p>
             <span className="font-light">3 dias pra entrega</span>
           </div>
 
-          <div>
+          <div className="mb-2">
             <p className="text-slate-400">Valor</p>
             <span className="font-light">R$ 4500,00</span>
           </div>
 
-          <div className="bg-green-200 rounded-3xl w-40 max-w-xs text-center py-1">
+          <div className="bg-green-200 rounded-3xl mb-2 w-40 max-w-xs text-center py-1">
             <span className="text-green-500 font-bold">em andamento</span>
           </div>
 
@@ -63,23 +63,23 @@ const Dashboard = () => {
 
         </div>
 
-        <div className="bg-slate-100 flex flex-col md:flex-row space-x-4 justify-between items-center p-4 rounded-md mb-4">
-          <div>
-            <span className="mx-4 text-slate-400 ">1</span>
+        <div className="bg-slate-100 flex flex-col md:flex-row md:space-x-4 md:justify-between items-center p-4 rounded-md mb-4">
+          <div className="mb-2">
+            <span className="md:mx-4 px-1 text-slate-400 ">1</span>
             <span className="font-bold">Pizzaria Guloso</span>
           </div>
 
-          <div>
+          <div className="mb-2">
             <p className="text-slate-400">Prazo</p>
             <span className="font-light">3 dias pra entrega</span>
           </div>
 
-          <div>
+          <div className="mb-2">
             <p className="text-slate-400">Valor</p>
             <span className="font-light">R$ 4500,00</span>
           </div>
 
-          <div className="bg-red-200 rounded-3xl w-40 max-w-xs text-center py-1">
+          <div className="bg-red-200 rounded-3xl w-40 mb-2 max-w-xs text-center py-1">
             <span className="text-red-500 font-bold">Encerrado</span>
           </div>
 
@@ -87,7 +87,6 @@ const Dashboard = () => {
             <div className="border border-slate-300 p-1 rounded-md hover:bg-violet-600 hover:text-white"><MdBorderColor size={20} /></div>
             <div className="border border-slate-300 p-1 rounded-md hover:bg-violet-600 hover:text-white"><MdOutlineDelete size={20} /></div>
           </div>
-
         </div>
       </div>
     </>
